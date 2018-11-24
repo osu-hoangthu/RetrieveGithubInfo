abe356bd6d30bbfd87fd665aca020305af7eaf6f
osu-hoangthu <hoangthu@oregonstate.edu>
2018-11-23 17:03:33 -0800

    add ability to get commit information from Github

--- getGithubAPIJson.py
+++ getGithubAPIJson.py
@@ -7,9 +7,9 @@ if  len(sys.argv) <= 1:
     print("No link given")
     exit()
 
-githublink =  str(sys.argv[1])
+githubRepo =  str(sys.argv[1])
 
-githublink = "https://api.github.com/repos/" + githublink
+githublink = "https://api.github.com/repos/" + githubRepo
 
 print("concatenated githublink: " + githublink)
 
@@ -27,23 +27,51 @@ numIssues = data['open_issues']
 license = data['license']
 hasLicense = False
 githubURL = data['url']
-if license != '/0':
+if str(license) is None:
     hasLicense = True
     licenseName = data['license']['name']
 pushedDate = data['pushed_at']
 
 if DEBUG:
-    print("name: " + name)
-    print("node ID: " + id)
-    print("creation date: " + creationDate)
-    print("update date: "  + updateDate)
-    print("num stars: " + str(numStars))
-    print("num watchers: " + str(numWatchers))
-    print("num forks: " + str(numForks))
-    print("number of issues: " + str(numIssues))
-    print("url: " + githubURL)
+    print("\tname: " + name)
+    print("\tnode ID: " + id)
+    print("\tcreation date: " + creationDate)
+    print("\tupdate date: "  + updateDate)
+    print("\tnum stars: " + str(numStars))
+    print("\tnum watchers: " + str(numWatchers))
+    print("\tnum forks: " + str(numForks))
+    print("\tnumber of issues: " + str(numIssues))
+    print("\turl: " + githubURL)
     if hasLicense:
-        print(licenseName)
-    print("latest commit: " + pushedDate)
+        print("\t" + licenseName)
+    print("\tlatest commit: " + pushedDate)
+
+commitsLink = "https://api.github.com/repos/" + githubRepo + "/commits"
+print("github commit link: " + commitsLink)
+
+with urllib.request.urlopen(commitsLink) as url:
+    commitData = json.loads(url.read().decode())
+
+numCommits = len(commitData)
+commitSHA = []
+authors = []
+commitMsg = []
+commitDates = []
+i = 0
+
+for i in range(numCommits):
+    commitSHA.append(commitData[i]['sha'])
+    authors.append(commitData[i]['commit']['author']['name'])
+    commitMsg.append(commitData[i]['commit']['message'])
+    commitDates.append(commitData[i]['commit']['author']['date'])
+    i += 1
+
+if DEBUG:
+    print("\tnumber of commits: " + str(numCommits))
+    print("\tSha:" + str(commitSHA))
+    print("\tAuthor: " + str(authors))
+    print("\tcommit messages: " + str(commitMsg))
+    print("\tdate: " + str(commitDates))
+
 
 print("done")
